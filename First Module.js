d695dd4f0adb6094e8e84ee15def3c554986cc69
santiagogomez102 <santiago.gomez102@outlook.com>
2024-02-17 12:38:32 -0500

    1.3.3
    A function as a property type - a method

--- First Module.js
+++ First Module.js
@@ -1,14 +1,15 @@
 "use strict"
 
-let test = {
-    nr: 10,
-    b: false,
-    str: "uno dos tres",
-    arr: [10, 20, 30],
-    obj: { x: 10, y: 20},
-    fn: function(arg) {console.log(arg)}
-};
-test.fn(123);
-
-console.log(test.obj.x);
-test.obj.y = 40;
+let point = {
+    x: 0,
+    y: 0,
+    moveHorizontally: function(distance) {
+        this.x = this.x + distance;
+    },
+    moveVertically: function(distance) {
+        this.y = this.y + distance;
+    }
+}
+console.log(point.x);
+point.moveHorizontally(30);
+console.log(point.x);
