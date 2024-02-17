0fd5a3f9fdc08e53f8a164b1b5ad64a30b79ca71
santiagogomez102 <santiago.gomez102@outlook.com>
2024-02-17 13:06:32 -0500

    1.4 Bracket notation

--- First Module.js
+++ First Module.js
@@ -19,3 +19,23 @@ console.log(contact.email.work);
 delete contact.email.work;
 console.log(contact.email.work);
 console.log(contact.email.private);
+
+contact.tel === contact["tel"];                     //Same thing, different notation
+contact.email.work === contact["email"]["work"];
+
+let contact1 = {
+    "first name": "Ronald"
+};
+contact1["first name"] = "Tim";
+// contact.first name = "Tim"; ----> This notation doesn't work
+//contact."first name" = "Tim"----> This notation doesn't work
+console.log(contact1["first name"]);
+
+let contact2 = {
+    email_1: "RonaldSMurphy@freepost.org",
+    email_2: "rsmurphy@briazz.com"
+}
+for(let i = 1; i <= 2; i++) {
+    let key = "email_" + i;
+    console.log(`${key}: ${contact2[key]}`);
+}
