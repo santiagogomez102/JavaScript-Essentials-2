ec93801b417e04b9fc9554373f932f1dcc381a1f
santiagogomez102 <santiago.gomez102@outlook.com>
2024-02-17 12:54:20 -0500

    1.3.6
    Add, modify and delete properties

--- First Module.js
+++ First Module.js
@@ -1,15 +1,21 @@
 "use strict"
 
-let point = {
-    x: 0,
-    y: 0,
-    moveHorizontally: function(distance) {
-        this.x = this.x + distance;
-    },
-    moveVertically: function(distance) {
-        this.y = this.y + distance;
-    }
-}
-console.log(point.x);
-point.moveHorizontally(30);
-console.log(point.x);
+let contact = {
+    tel: "207-662-5412",
+    email: "RonaldSMurphy@freepost.org"
+};
+contact.firstName = "Ronald";
+contact.lastName = "Murphy";
+console.log(contact.tel);
+console.log(contact.firstName);
+console.log(contact.notes);
+contact.email = ["RonaldSMurphy@freepost.org", "rsmurphy@briazz.com"];
+console.log(contact.email);
+contact.email = {
+    private: "RonaldSMurphy@freepost.org",
+    work: "rsmurphy@briazz.com"
+};
+console.log(contact.email.work);
+delete contact.email.work;
+console.log(contact.email.work);
+console.log(contact.email.private);
