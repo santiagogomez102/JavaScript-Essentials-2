72cc5c2dbe404d87c2c8fcdfbf8942ce7feca16c
santiagogomez102 <santiago.gomez102@outlook.com>
2024-02-20 14:54:54 -0500

    1.6.2 The "const" object can be modified

--- First Module.js
+++ First Module.js
@@ -1,73 +1,13 @@
 "use strict"
 
-let contact = {
-    tel: "207-662-5412",
-    email: "RonaldSMurphy@freepost.org"
-};
-contact.firstName = "Ronald";
-contact.lastName = "Murphy";
-console.log(contact.tel);
-console.log(contact.firstName);
-console.log(contact.notes);
-contact.email = ["RonaldSMurphy@freepost.org", "rsmurphy@briazz.com"];
+const x = 10;
+// x = 20 can't be done as x is "const", not "let"
+
+const contact = {};
+// contact = { email: "RonaldSMurphy@freepost.org"}; can't be done as contact is "const", not let
+contact.email = "RonaldSMurphy@freepost.org";
 console.log(contact.email);
-contact.email = {
-    private: "RonaldSMurphy@freepost.org",
-    work: "rsmurphy@briazz.com"
-};
-console.log(contact.email.work);
-delete contact.email.work;
-console.log(contact.email.work);
-console.log(contact.email.private);
-
-contact.tel === contact["tel"];                     //Same thing, different notation
-contact.email.work === contact["email"]["work"];
-
-let contact1 = {
-    "first name": "Ronald"
-};
-contact1["first name"] = "Tim";
-// contact.first name = "Tim"; ----> This notation doesn't work
-//contact."first name" = "Tim"----> This notation doesn't work
-console.log(contact1["first name"]);
-
-let contact2 = {
-    email_1: "RonaldSMurphy@freepost.org",
-    email_2: "rsmurphy@briazz.com"
-}
-for (let i = 1; i <= 2; i++) {
-    let key = "email_" + i;
-    console.log(`${key}: ${contact2[key]}`);
-}
-
-// Check if contact.notes exist
-
-if (!contact.notes) {
-    contact.notes = "something really important";
-}
-console.log(contact.notes)
-
-if (contact && contact.email){
-    console.log(contact.email.private);
-}
-
-contact && contact.email && console.log(contact.email.private);
-
-if ("notes" in contact) {
-    console.log(contact.notes);
-}
-
-for (let x in contact) {
-    console.log(x);
-}
-
-for (let x in contact) {
-    console.log(contact[x]);
-}
-
-for (let x in contact) {
-    console.log(`${x}: ${contact[x]}`);
-}
-
-let keys = Object.keys(contact);
-console.log(keys);
+contact.email = "rsmurphy@briazz.com";
+console.log(contact.email);
+delete contact.email;
+console.log(contact.email);
