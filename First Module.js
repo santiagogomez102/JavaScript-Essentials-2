c66a477b8213373d8038ba0c600134b0ee61dd1b
santiagogomez102 <santiago.gomez102@outlook.com>
2024-02-20 16:22:05 -0500

    1.7.4 Getters and setters

--- First Module.js
+++ First Module.js
@@ -1,20 +1,24 @@
 "use strict"
 
-let circle = {
-    radius: 100,
-    center: {
-        x: 0,
-        y: 0,
-        show(){console.log(`${this.x}, ${this.y}`)}
-    }
-}
-circle.center.show();
+let contact = {
+    _tel: "207-662-5412",
+    get tel() {return this._tel;},
+    set tel(t) {this._tel = t;}
+};
+console.log(contact.tel);
+contact.tel = "100-100-1000";
+console.log(contact.tel);
+contact.email = "RonaldSMurphy@freepost.org";
+console.log(contact.email);
 
-let test = {
-    point: circle.center
-}
-test.point.show();
-
-circle.center.x = 10;
-circle.center.show();
-test.point.show();
+let contact1 = {
+    _age: 36,
+    firstName: "David",
+    lastName: "Taylor",
+    get fullName() {return `${this.firstName} ${this.lastName}`;},
+    get age() {return this._age;},
+    set age(a) {if (a > 0) this._age = a;}
+};
+console.log(contact1.fullName);
+contact.age = -20;
+console.log(contact1.age);
