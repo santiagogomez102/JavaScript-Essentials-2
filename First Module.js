349879f723d9bf7d316d62e3aae26171aa403935
santiagogomez102 <santiago.gomez102@outlook.com>
2024-02-26 15:20:24 -0500

    1.8
    Property and object configuration

--- First Module.js
+++ First Module.js
@@ -1,24 +1,49 @@
 "use strict"
 
 let contact = {
-    _tel: "207-662-5412",
-    get tel() {return this._tel;},
-    set tel(t) {this._tel = t;}
-};
-console.log(contact.tel);
-contact.tel = "100-100-1000";
-console.log(contact.tel);
-contact.email = "RonaldSMurphy@freepost.org";
-console.log(contact.email);
-
-let contact1 = {
     _age: 36,
     firstName: "David",
     lastName: "Taylor",
     get fullName() {return `${this.firstName} ${this.lastName}`;},
     get age() {return this._age;},
-    set age(a) {if (a > 0) this._age = a;}
+    set age(a) {if( a > 0) this._age = a;}
 };
-console.log(contact1.fullName);
-contact.age = -20;
-console.log(contact1.age);
+let keys = Object.keys(contact);
+console.log(keys);
+
+let desc = Object.getOwnPropertyDescriptor(contact, "firstName");
+console.log(desc);
+
+let contact1 = {};
+Object.defineProperty(contact1, "_age", {
+    value: 36,
+    writable: true,
+    enumerable: false,
+    configurable: true
+});
+let keys1 = Object.keys(contact1);
+console.log(keys1);
+contact1._age = 100;
+console.log(contact1._age);
+
+Object.defineProperty(contact1, "_age", {
+    value: contact1._age,
+    writable: false,
+    enumerable: false,
+    configurable: true
+});
+console.log(contact1._age);
+// contact1._age = 200;   --> Error as property is not writable
+console.log(contact1._age);
+
+let enumKeys = Object.keys(contact1);
+let allKeys = Object.getOwnPropertyNames(contact1);
+console.log(enumKeys);
+console.log(allKeys);
+
+Object.preventExtensions(contact1);     //Can't add new properties
+Object.seal(contact1);                  //Can't add, remove or reconfigure properties
+Object.freeze(contact1);                //Similar to seal, additionally can't change values of properties
+Object.isExtensible(contact1);
+Object.isSealed(contact1);
+Object.isFrozen(contact1);
